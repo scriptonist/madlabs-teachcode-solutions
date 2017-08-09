c7f26b7d3a22d3e9e2c07a71a96cd738da13651d
scriptonist <aravidkp@outlook.in>
2017-08-09 19:06:30 +0530

    14Task Submitted

---
+++ solution1415.py
@@ -0,0 +1,21 @@
+numOne = 101
+numTwo = 111
+
+result = numOne  & numTwo
+print(result)
+
+result = numOne | numTwo
+print(result)
+
+result = ~numTwo
+print(result)
+
+result = numOne ^  numTwo
+print(result)
+
+result = numOne << numTwo
+print(result)
+
+result = numOne >>  numTwo
+print(result)
+
